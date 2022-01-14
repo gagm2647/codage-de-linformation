1b45d6d33437004788b9b1018e123670e50bc349
Mathieu Gagnon <gagm2647@usherbrooke.ca>
2022-01-14 08:40:41 -0500

    Project creation

---
+++ laboratoire.py
@@ -0,0 +1,7 @@
+import scipy as sc
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def num_1():
+    return 0
---
+++ main.py
@@ -0,0 +1,11 @@
+import laboratoire
+
+if __name__ == '__main__':
+    print("Test script")
+
+    to_run = 1
+
+    if to_run == 1:
+        laboratoire.num_1()
+    else:
+        print("Not running anything")
